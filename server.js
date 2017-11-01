b9dea8ba1970832490c0e2e3c0091f4cbdbefbb4
KiranMantha <kiranv.mantha@gmail.com>
2017-11-01 13:22:35 +0530

    setup dotenv and mongoose

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // db setup
-mongoose.connect(process.env.MONGO_URL_LOCAL);
+var options = {
+  useMongoClient: true,
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+mongoose.connect(process.env.MONGO_URL_LOCAL, options);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
